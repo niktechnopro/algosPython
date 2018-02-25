b72df6c56f0787dd28f8201b6c86781cab087dc0
Nikolas Bogucarskis <niktechnopro@gmail.com>
2018-02-25 06:53:03 -0500

    more elegant solution

--- stringMethods.js
+++ stringMethods.js
@@ -1,19 +1,36 @@
 // The includes() method determines whether a string contains the characters of a specified string.
 // This method returns true if the string contains the characters, and false if not.
 
+// function vowelsAndConsonants(s) {
+//     let vowels = ['a','e','i','o','u']
+//     let vowels1 = 'aeiou'
+//     for (let i=0; i<s.length; i++){
+//         if (vowels.includes(s[i])){
+//             console.log(s[i])
+//          }
+//     }
+//     for (i of s){//another way to iterate through loop
+//         if (!vowels.includes(i)){
+//             console.log(i)
+//          }
+//     }
+// }
+
+// vowelsAndConsonants('javascriptloops');
+
+//more elegant solution with 1 loop
 function vowelsAndConsonants(s) {
-    let vowels = ['a','e','i','o','u']
-    let vowels1 = 'aeiou'
-    for (let i=0; i<s.length; i++){
-        if (vowels.includes(s[i])){
-            console.log(s[i])
-         }
+    let vowels = ['a','e','i','o','u'];
+    let consonants = '';
+    let vowels1 = ''
+    for (i of s){
+        if (vowels.includes(i)){
+          vowels1 += i + '\n'
+        }else{
+          consonants += i + '\n'
+        }
     }
-    for (i of s){//another way to iterate through loop
-        if (!vowels.includes(i)){
-            console.log(i)
-         }
-    }
+    return (vowels1 + consonants);
 }
 
-vowelsAndConsonants('javascriptloops');
+console.log(vowelsAndConsonants('javascriptloops'))
