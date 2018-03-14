29523020db56f74f2daa7a478f045244c320238c
ciscoscholar <ciscoscholar@gmail.com>
2018-03-14 15:38:57 -0400

    fix

--- codingChallenges/characterCountWebPage/JS/letterCountCrawler.js
+++ codingChallenges/characterCountWebPage/JS/letterCountCrawler.js
@@ -4,42 +4,44 @@ var bttn = document.querySelector('[name="button"]');
 bttn.addEventListener('click', validation);
 
 function validation(){
-	var userInput = document.querySelector('[name="inputText"]').value.trim();
-	if (userInput == ''){
+    var userInput = document.querySelector('[name="inputText"]').value.trim();
+    if (userInput == ''){
         console.log('field is empty');
+        bttn.removeEventListener('click', validation);
         $('[name="inputText"]').val('this field can not be empty').css('color', 'red');
         $('[name="inputText"]').on('focus', function(){
             $(this).val('').css('color', 'black')
-       	})
+            bttn.addEventListener('click', validation);
+        })
     }else{
-    	let ulel = document.querySelector('ul');
-    	if (ulel){
-    		console.log('removing <ul> ulelement');
-    		ulel.remove();
-    	} 
-    	let count = getCount(userInput);
-    	presentation(count, userInput);
-    	$('[name="inputText"]').val('').css('color', 'black');
+        let ulel = document.querySelector('ul');
+        if (ulel){
+            console.log('removing <ul> ulelement');
+            ulel.remove();
+        } 
+        let count = getCount(userInput);
+        presentation(count, userInput);
+        $('[name="inputText"]').val('').css('color', 'black');
     }
 }
 
 function fetching(userInput){
-	// var url = "http://crossorigin.me/" + "https://" + (userInput);
-	var url = "https://my-little-cors-proxy.herokuapp.com/" + "https://" + (userInput);
-	// console.log('getting url from the field', url);
-	var promiseOne = $.get(url)
-	promiseOne.then(result => parser(result));
+    // var url = "http://crossorigin.me/" + "https://" + (userInput);
+    var url = "https://my-little-cors-proxy.herokuapp.com/" + "https://" + (userInput);
+    // console.log('getting url from the field', url);
+    var promiseOne = $.get(url)
+    promiseOne.then(result => parser(result));
 }
 
 function parser(data){
-	var parser = new DOMParser();
-	var doc = parser.parseFromString(data, "text/html");
-	console.log(doc);
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(data, "text/html");
+    console.log(doc);
 }
 
 function getCount(string) {
-	var letterCount = {};
-	var avoid = [' ']; //leave it just in case if later want to filter some characters
+    var letterCount = {};
+    var avoid = [' ']; //leave it just in case if later want to filter some characters
     // var avoid = [',', '/', ' ', '&', '!', '<', '>', '?']//array of characters to ignore
     for (var i=0; i<string.length;i++) {
       var letter = string.charAt(i);
@@ -51,14 +53,14 @@ function getCount(string) {
 };
 
 function presentation(count, userInput){
-	console.log('presentation envoked', count, userInput);
-	var keys = Object.keys(count);
-	var article = document.querySelector('article');
-	var ulElement = document.createElement('ul');
-	var stringEl = document.createTextNode(`for string: ${userInput}`);
-	ulElement.appendChild(stringEl);
-	article.appendChild(ulElement);
-	keys.forEach((key)=>{
+    console.log('presentation envoked', count, userInput);
+    var keys = Object.keys(count);
+    var article = document.querySelector('article');
+    var ulElement = document.createElement('ul');
+    var stringEl = document.createTextNode(`for string: ${userInput}`);
+    ulElement.appendChild(stringEl);
+    article.appendChild(ulElement);
+    keys.forEach((key)=>{
         let liElement = document.createElement('li');
         ulElement.appendChild(liElement);
         let textRow = document.createTextNode(`'${key}' => ${count[key]}`)
@@ -66,5 +68,3 @@ function presentation(count, userInput){
     })
 
 }
-
-
