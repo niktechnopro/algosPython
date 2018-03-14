263930e39afaa5b24a644725a683642d8a9f3bb5
Nikolas Bogucarskis <niktechnopro@gmail.com>
2018-03-14 08:25:24 -0400

    practice

--- codingChallenges/Fibonacci/fibChallenge.js
+++ codingChallenges/Fibonacci/fibChallenge.js
@@ -1,22 +1,22 @@
 // 0, 1, 1, 2, 3, 5, 8, 13... 
 // considering that first number=0, second=1, 
-// third=1, fourth=2, etc
+// third=1, fourth=2, fifth=3, etc...
 
 
 var fib = function(n) {
   //so a=0, b=1; and c is an element we are interested in
-  var a = 0, b = 1, c;
+  var first = 0, second = 1, current;
   if (n < 0) c = "must be 0 or positive number";
-  else if(n == 1) c = 0;
-  else if(n <= 2) c = 1;
+  else if(n == 1) current = 0;
+  else if(n <= 2) current = 1;
   else{
     for (let i = 3; i<=n; i++){
-      c = a + b; // the next number is sequence is a sum of 2 previous numbers
-      a = b
-      b = c;
+      current = first + second; // the next number is sequence is a sum of 2 previous numbers
+      first = second;
+      second = current;
     }
   }
-  return c
+  return current;
 };
 
 console.log(fib(5)); //fifth element should return 3
