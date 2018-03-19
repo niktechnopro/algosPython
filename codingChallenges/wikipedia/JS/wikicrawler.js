1a9770cad103c7c124d9a5fb686871995d73b625
ciscoscholar <ciscoscholar@gmail.com>
2018-03-19 11:33:46 -0400

    wikipedia

--- codingChallenges/wikipedia/JS/wikicrawler.js
+++ codingChallenges/wikipedia/JS/wikicrawler.js
@@ -1,6 +1,5 @@
-console.log('letter count module');
+console.log('sanity check')
 var bttn = document.querySelector('[name="button"]');
-
 bttn.addEventListener('click', validation);
 
 function validation(){
@@ -17,26 +16,60 @@ function validation(){
         let ulel = document.querySelector('ul');
         if (ulel){
             console.log('removing <ul> ulelement');
+            totalsum = '';
             ulel.remove();
         } 
-        let count = getCount(userInput);
-        presentation(count, userInput);
+     	// console.log('userInput', userInput);
+     	var results = fetcher(userInput);
         $('[name="inputText"]').val('').css('color', 'black');
     }
 }
 
-function fetching(userInput){
-    // var url = "http://crossorigin.me/" + "https://" + (userInput);
-    var url = "https://my-little-cors-proxy.herokuapp.com/" + "https://" + (userInput);
-    // console.log('getting url from the field', url);
-    var promiseOne = $.get(url)
-    promiseOne.then(result => parser(result));
+function fetcher(userInput){
+	console.log('we are in fetcher function', userInput)
+	var apiUrl = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
+	var remoteUrlWithOrigin = apiUrl + userInput;
+	fetch( remoteUrlWithOrigin, {
+	    method: 'POST',
+	    headers: new Headers( {
+	        'Api-User-Agent': 'Example/1.0'
+	    } )
+    // Other init settings such as 'credentials'
+	} ).then( function ( response ) {
+	    if ( response.ok ) {
+	        return response.json();
+	    }
+	    throw new Error( 'Network response was not ok: ' + response.statusText );
+	} ).then( function ( data ) {
+    	// do something with data
+    	let pagess = data.query.pages;
+    	parsingData(pagess, userInput);
+	});
 }
 
-function parser(data){
-    var parser = new DOMParser();
-    var doc = parser.parseFromString(data, "text/html");
-    console.log(doc);
+function parsingData(pagess, userInput){
+	console.log(pagess)
+	let fullString = '';
+	let keys = Object.keys(pagess)
+    	console.log(keys)
+    	keys.forEach((pageid, index)=>{
+    		var string = infoExtraction(pagess[pageid], userInput);
+    		fullString += string;
+    	})
+    let count = getCount(fullString)
+    presentation(count, userInput)
+}
+
+function infoExtraction(info, userInput){
+	var totalsum = '';
+	var title = info.title;
+	var body = info.extract;
+	var info = {
+		title,
+		body
+	}
+	totalsum = title + body;
+	return totalsum
 }
 
 function getCount(string) {
@@ -52,13 +85,12 @@ function getCount(string) {
   return letterCount;
 };
 
-
 function presentation(count, userInput){
     console.log('presentation envoked', count, userInput);
     var keys = Object.keys(count);
     var article = document.querySelector('article');
     var ulElement = document.createElement('ul');
-    var stringEl = document.createTextNode(`for string: ${userInput}`);
+    var stringEl = document.createTextNode(`for wikipedia on: ${userInput}`);
     ulElement.appendChild(stringEl);
     article.appendChild(ulElement);
     keys.forEach((key)=>{
@@ -68,3 +100,6 @@ function presentation(count, userInput){
         liElement.appendChild(textRow);
     })
 }
+
+
+
