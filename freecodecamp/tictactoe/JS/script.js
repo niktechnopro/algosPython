1d74d98395f96cf5f06b12c6c2b434a646ddb8e9
ciscoscholar <niktechnopro@gmail.com>
2018-03-22 22:03:12 -0400

    first commit

---
+++ freecodecamp/tictactoe/JS/script.js
@@ -0,0 +1,98 @@
+console.log('sanity check');
+//global variables
+var turn = 0;
+var fields = new Array(9).fill(1); //an array to keep track of fields
+console.log(fields)
+var board = Array.from(Array(9).keys()); //creating array for combinations of X and O
+console.log(board)
+const winCombos = [//this are indexes of squares for winning combinations
+	[0,1,2],
+	[3,4,5],
+	[6,7,8],
+	[0,3,6],
+	[1,4,7],
+	[2,5,8],
+	[0,4,8],
+	[2,4,6]
+]
+var playField = document.querySelector('#structure');
+
+
+playField.addEventListener('click', userTurn);
+
+
+var theMessage = function(msg){
+	document.getElementById('messages').textContent=`${msg}`;
+	return	
+}
+
+var playerMove =function(squareid){
+	fields.pop()
+	var moveX = document.getElementById(squareid).textContent='X';
+	theMessage("O's turn now");
+	console.log(squareid)
+	board[squareid] = moveX;
+	winningCombination(board);
+}
+
+function computerMove(){
+	console.log('computer turn');
+	console.log(fields)
+		if (fields.length > 0){
+			var randomCell = getRandomInt(9);
+			var isEmpty = emptySpaceCheck(randomCell);
+			if (!isEmpty){
+				computerMove();
+			}else{
+				var moveO = document.getElementById(randomCell).textContent='O';
+				theMessage("X's turn now")
+				fields.pop();
+				board[randomCell] = moveO;
+				playField.addEventListener('click', userTurn);
+				winningCombination(board)
+		}
+		}else{
+			console.log(fields)
+			theMessage("	Draw!!! - Good Game")
+			return
+	}
+	
+}
+
+function userTurn(e){
+	var squareid = e.target.id;//reading value of the square
+	var emptySpace = emptySpaceCheck(squareid);
+	if (emptySpace){
+		playerMove(squareid);
+		setTimeout(computerMove, 1000)
+		playField.removeEventListener('click', userTurn)
+	}else{
+		theMessage('pick another square');
+	}
+}
+
+function emptySpaceCheck(squareid){
+	var emptySpace = document.getElementById(squareid).textContent;
+	console.log(squareid, emptySpace)
+	if (!emptySpace){
+		console.log('its empty')
+		return true
+	}else{
+		console.log('not empty')
+		return false
+	}	
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function winningCombination(boad){
+	//if X or O are placed in any of winCombos
+	
+	
+}
+
+var theGame = function(){
+	theMessage("you are starting with 'X'")
+}();
