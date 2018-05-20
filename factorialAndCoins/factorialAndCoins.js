ba3d54a3425942596ad9179e7717ddae6478759c
ciscoscholar <niktechnopro@gmail.com>
2018-05-19 23:11:40 -0400

    factorial and coins

---
+++ factorialAndCoins/factorialAndCoins.js
@@ -0,0 +1,38 @@
+// factorial function
+function factorial(n){//factorial is a product of n!
+  let sum=1
+  for (let i=n; i >0; i--){
+    sum *= i;
+  }
+  return sum
+}
+console.log(factorial(4))
+//coin converter
+//87 cents should convert into 25,25,25,10,2
+//let's create an array of possible coins
+let coins = [25,10,5,1];
+let coinConverter = (amount) =>{
+  let total = amount;
+  let accumulator = new Array();
+  while (amount > 0){
+    if (amount - 25 > 0){
+      amount -= 25;
+      accumulator.push(25);
+    }else if(amount - 10 > 0){
+      amount -=10;
+      accumulator.push(10);
+    }else if(amount - 5 > 0){
+      amount -= 5;
+      accumulator.push(5)
+    }else if(amount -1 > 0){
+      amount -= 1;
+      accumulator.push(1)
+    }else if(amount - 1 === 0){
+      accumulator.push(1)
+      break;
+    }
+  }
+  return `to break ${total} cents only needed the following coins: ${accumulator}`;
+}
+
+console.log(coinConverter(84))
