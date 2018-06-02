c5f62aac231b0435bee90c2523e13f028d1b8591
ciscoscholar <niktechnopro@gmail.com>
2018-06-02 17:07:56 -0400

    few more updates

--- binary_calculator/JS/binaryCalculator.js
+++ binary_calculator/JS/binaryCalculator.js
@@ -1,1 +1,17 @@
 // alert('Sanity check');
+//first let's make a listener for buttons
+let btns = document.querySelector('#btns');
+let display = document.querySelector('[name=display]')
+//let's add an event listener
+btns.addEventListener('click', valueReader);//where valueReader handles value
+
+function valueReader(event) {
+	if (event.target.innerText !== undefined){
+		display.value = event.target.innerText;
+		calcLogic(event.target.innerText)
+	}
+}
+
+function calcLogic(value){
+	console.log(value)
+}
