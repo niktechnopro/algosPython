595fdb08187de5d7a73ef309e5b39882e6c66d46
ciscoscholar <niktechnopro@gmail.com>
2018-07-22 21:15:01 -0400

    image uploader test

--- freecodecamp/productLandingPage/JS/script.js
+++ freecodecamp/productLandingPage/JS/script.js
@@ -1,1 +1,73 @@
-alert('test')
+//now we are going to test our image uploader before creating component in react
+let submit = document.querySelector('[name="submit"]');
+let preview = document.querySelector('#image_tag');
+submit.addEventListener('click', imageFile);
+
+function imageFile(){
+	file = document.getElementById("file_uploader").files[0];
+	fileProcessor(file).then((result) => {
+		console.log(result)
+		return canvasWorks(result)
+	})
+}
+
+
+function fileProcessor(file){
+	console.log('we are processing the file');
+	let reader = new FileReader();
+	if (reader){
+		return new Promise((resolve, reject)=> {	
+			reader.onload = function(event) {
+			    // The file's text will be printed here
+			    // console.log(event.target.result)
+			    preview.src = reader.result;
+		  	};
+		  	reader.onloadend = function(event){
+		  		resolve(reader.result)
+		  	}
+		  	reader.onerror = function(event){
+		  		preview.src = "";
+		  		alert('there was a problem with the image')
+		  		reject(reader.error)
+		  	}
+		  	reader.readAsDataURL(file);
+		})
+	}else{
+		alert('Your browser does not support our image uploader')
+	}
+	
+}
+
+
+function canvasWorks(img){
+	//first let's create invisible image element and attach it to anything
+	let imageCopy = new Image();//creates new <img> element
+	document.body.appendChild(imageCopy);
+	imageCopy.setAttribute('style', 'display: none');
+	imageCopy.setAttribute('alt','script div');
+    imageCopy.setAttribute("src", img);
+    imageCopy.setAttribute('id', 'copy')
+
+    //let's set the Canvas next
+    if (imageCopy){
+	    let imgCanvas = document.createElement("canvas"),
+	    imgContext = imgCanvas.getContext("2d");
+
+	//let's calculate the right ratio for desired screen, desired width=750px  and height=450px
+	    let ratio = Math.min(750/imageCopy.width, 450/imageCopy.height)
+	    imgCanvas.width = imageCopy.width*ratio;
+	    imgCanvas.height = imageCopy.height*ratio;
+
+	    console.log(imgCanvas.width, imgCanvas.height, ratio)
+    // Draw image into canvas element
+    	imgContext.drawImage(imageCopy, 0, 0, imgCanvas.width, imgCanvas.height);
+
+    // Read information from Canvas
+    	imgInfo = imgCanvas.toDataURL('image/png');
+    	console.log(imgInfo)
+    	// console.log(imgInfo);
+    	preview.src = imgInfo;
+    	document.body.removeChild(imageCopy);
+    }
+}
+
