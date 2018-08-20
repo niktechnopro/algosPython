edc8ddba3a5ef256191b3f5695af06db9732077b
ciscoscholar <niktechnopro@gmail.com>
2018-08-19 23:31:40 -0400

    playing with slider

--- Product_Landing_Page/JS/script.js
+++ Product_Landing_Page/JS/script.js
@@ -1,1 +1,8 @@
 // alert('sanity check')
+// let sli = document.querySelector('#slider');
+// console.log(sli.value)
+// sli.addEventListener('onInput', (e)=>console.log('moving', e, sli.value))
+
+function moving(e){
+	console.log('moving', e)
+}
