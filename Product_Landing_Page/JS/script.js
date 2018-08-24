bd4c5e121e413695b03c06fe11505852f0aa8783
ciscoscholar <niktechnopro@gmail.com>
2018-08-23 21:10:54 -0400

    some progress

--- Product_Landing_Page/JS/script.js
+++ Product_Landing_Page/JS/script.js
@@ -1,8 +1,23 @@
 // alert('sanity check')
-// let sli = document.querySelector('#slider');
-// console.log(sli.value)
-// sli.addEventListener('onInput', (e)=>console.log('moving', e, sli.value))
+
+//defining constants:
+let image = document.querySelector('#slider-image');
+let width;
+let frame = document.querySelector('#frame');
+let frameHeight;
+
+if (image){
+	imageWidth = image.width;
+	let imageHeight = image.height;
+	frameHeight = frame.height;
+	console.log(frame, frameHeight)
+}
 
 function moving(e){
-	console.log('moving', e)
+
+	console.log(e, imageWidth)
+	//changing image width
+	let newWidth = e/100 * imageWidth;
+	image.width = newWidth;
+	frame.setAttribute('height', frameHeight)
 }
