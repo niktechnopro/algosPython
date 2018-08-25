e75d276a716b7a63db9dce015bdc75e12155b8e6
ciscoscholar <niktechnopro@gmail.com>
2018-08-24 23:37:33 -0400

    some progress

--- Product_Landing_Page/JS/script.js
+++ Product_Landing_Page/JS/script.js
@@ -2,22 +2,27 @@
 
 //defining constants:
 let image = document.querySelector('#slider-image');
-let width;
-let frame = document.querySelector('#frame');
-let frameHeight;
 
-if (image){
+let imagePromise = new Promise((resolve, reject)=>{
 	imageWidth = image.width;
 	let imageHeight = image.height;
-	frameHeight = frame.height;
-	console.log(frame, frameHeight)
-}
+	if (image){
+		resolve({
+		width : imageWidth,
+		height : imageHeight
+		})
+	}
+})
+
+imagePromise.then((result) => {
+	let width='boo'
+	let imageWrapper = document.querySelector('#image_wrapper')
+	imageWrapper.width=result.width;
+})
+
 
 function moving(e){
-
-	console.log(e, imageWidth)
 	//changing image width
 	let newWidth = e/100 * imageWidth;
 	image.width = newWidth;
-	frame.setAttribute('height', frameHeight)
 }
