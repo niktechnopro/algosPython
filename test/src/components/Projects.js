c5f1a42478dd7bfe0919fabbe45e2fe148e154c6
ciscoscholar <niktechnopro@gmail.com>
2018-09-16 19:46:09 -0400

    some

--- test/src/components/Projects.js
+++ test/src/components/Projects.js
@@ -4,21 +4,61 @@ import styled from "styled-components";
 function First() {
   return (
     <Wrapper>
-    <div id="cardWrapper">  
-      <div class="item-space">
+
+    <p className="title">Projects</p>
+		<hr />
+
+    <div className="cardWrapper">
+      <p>Full-Stack Project</p>  
+      <div className="item-space">
         <figure>
             <img src="./images/Portfolio/locdocscreenshot.png" alt="Loc Doc Screenshot" />
             <figcaption>
                 <h3>LocDoc</h3>
-                <span class="fixer">React Redux NodeJS<p>Geolocation API and Doctor API Project</p></span>
-                <a rel="nofolow" target="_blank" href="https://github.com/niktechnopro/LocDoc" class="button">GitHub</a>
+                <span className="fixer">React Redux NodeJS<p>Geolocation API and Doctor API Project</p></span>
+                <a rel="nofolow" target="_blank" href="https://github.com/niktechnopro/LocDoc" className="button">GitHub</a>
             </figcaption>
         </figure>
-        <p class="desc">
+        <p className="desc">
           LocDoc - A powerful tool that enables the user to find a local doctor
         </p>
       </div>
     </div>
+
+    <div className="cardWrapper">
+      <p>Front-End Project</p>  
+      <div className="item-space">
+        <figure>
+            <img src="./images/Portfolio/focus.png" alt="Focus! Screenshot" />
+            <figcaption>
+                <h3>Focus!</h3>
+                <span className="fixer">Front End Project<p>HTML, Bootstrap/CSS, jQuery</p></span>
+                <a rel="nofolow" target="_blank" href="https://github.com/niktechnopro/Focus" className="button">GitHub</a>
+            </figcaption>
+        </figure>
+        <p class="desc">
+          Focus! - A timer based on the Pomodoro time management technique that includes entertainment
+        </p>
+      </div>
+    </div>
+
+    <div className="cardWrapper">
+      <p className>Hackathon Project</p>  
+      <div className="item-space">
+        <figure>
+            <img src="./images/Portfolio/village.png" alt="Village scresnshot" />
+            <figcaption>
+                <h3>Focus!</h3>
+                <span class="fixer">Hackaton against violence Project <p>HTML, Bootstrap/CSS, jQuery, Node.js, Express, MySQL</p></span>
+                <a rel="nofolow" target="_blank" href="https://github.com/niktechnopro/Village" className="button">GitHub</a>
+            </figcaption>
+        </figure>
+        <p className="desc">
+          Village - A webapp that builds emotional intelligence in adolescent children
+        </p>
+      </div>
+    </div>
+
     </Wrapper>
   );
 }
@@ -28,9 +68,13 @@ const Wrapper = styled.div`
   // background-color: #00d38a;
   padding: 20px;
 
-  #cardWrapper{
+  .cardWrapper{
     margin: 0 auto;
-    max-width: 350px;
+    max-width: 400px;
+  }
+
+  .cardWrapper p{
+    text-align: center;
   }
 
   .item-space{
